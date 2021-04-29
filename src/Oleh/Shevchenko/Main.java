a402759bdfb49211ca67917d180e9b4d01890838
Oleh Shevchenko <olejhashevchenko96@gmail.com>
2021-04-29 18:00:18 +0300

    Fix grammar error

--- src/Oleh/Shevchenko/Main.java
+++ src/Oleh/Shevchenko/Main.java
@@ -2,13 +2,13 @@ package Oleh.Shevchenko;
 
 public class Main {
     public static final int LEFTPART = 999;
-    public static final int RIGTHPART = 999;
+    public static final int RIGHTPART = 999;
 
     public static void main(String[] args) {
         int count = 0;
 
         for (int i = 0; i < LEFTPART; i++) {
-            for (int j = 0; j < RIGTHPART; j++) {
+            for (int j = 0; j < RIGHTPART; j++) {
                 if (i / 100 % 10 + i / 10 % 10 + i % 10 == j / 100 % 10 + j / 10 % 10 + j % 10) {
                     count++;
                 }
